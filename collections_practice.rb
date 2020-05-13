d98f7a59b4a287c31778b14a3d009eac2db13866
Victor Williams <victor.williams1030@gmail.com>
2020-05-13 22:15:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,5 +22,6 @@ end
 # 'array.sort.reverse' takes [12, 4, 35] and returns [35, 12, 4] and not [35, 4, 12]. Idk why, just thought it was interesting
 
 def kesha_maker(array)
+  cash_money = []
   
 end
